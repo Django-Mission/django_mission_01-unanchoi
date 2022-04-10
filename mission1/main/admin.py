ef8c66588aea7763b4c5f3dfa28121b61d9b97b6
unanchoi <unusualc@likelion.org>
2022-04-11 00:24:05 +0900

    refactor : Customize post admin

--- mission1/main/admin.py
+++ mission1/main/admin.py
@@ -1,5 +1,19 @@
+from csv import list_dialects
+from encodings import search_function
 from django.contrib import admin
+from jmespath import search
 from main.models import Post,  Comment
 # Register your models here.
-admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ("id", "image", "content",
+                    "created_at", "view_count", "writer")
+    # list_editable =('content' , )
+    list_filter = ("created_at", )
+    search_fields = ('id', 'writer__username')
+    search_help_text = "게시판 번호, 작성자 검색이 가능합니다."
+
+
 admin.site.register(Comment)
